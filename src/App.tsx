994e132c07272663801c390911fbb2d0590ba3b7
PaulPufel <ppufel@web.de>
2024-11-28 11:56:09 +0100

    Add consultation 04

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.css';
 
 //lessons
 // import Lesson06 from './lessons/Lesson06/Lesson06';
-import Lesson07 from './lessons/Lesson07/Lesson07';
+// import Lesson07 from './lessons/Lesson07/Lesson07';
 
 //homeworks
 
 //consultations
+import Consultation04 from './consultations/Consultation04/Consultation04';
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson06 /> */}
-      <Lesson07 />
+      {/* <Lesson07 /> */}
+      <Consultation04 />
 
     </div>
   );
